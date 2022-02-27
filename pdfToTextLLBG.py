93dcceefbdb93528e81ea0ea91027eda4de0463e
bleyzer <guilhem.mazoyer@laposte.net>
2022-02-27 23:55:43 +0100

    + lecture des auteurs soit à partir des métadonnées, soit à partir des adresses email présentent sur la première page. + récupération de la première ligne du document quand le titre n'est pas présent dans les métadonnées.

--- pdfToTextLLBG.py
+++ pdfToTextLLBG.py
@@ -1,49 +1,72 @@
-import fitz,re,sys,os
+from lib2to3.pgen2.token import EQUAL
+import fitz, re, sys, os
 
 # Ouverture fichiers pdf dans repertoire
 files = os.listdir(sys.argv[1])
-files = filter(lambda f: f.endswith(('.pdf','.PDF')), files)
+files = filter(lambda f: f.endswith(('.pdf', '.PDF')), files)
 
 for file in files:
-    doc = fitz.open(sys.argv[1]+ '/'+file)
+    doc = fitz.open(sys.argv[1] + '/' + file)
 
-    page = doc.load_page(0) # Lecture de la premiere page TODO DOIT BOUCLER SUR TOUTE LES PAGES
+    page = doc.load_page(0)  # Lecture de la premiere page TODO DOIT BOUCLER SUR TOUTE LES PAGES
     dl = page.get_displaylist()
     tp = dl.get_textpage()
     tp_text = tp.extractText()
 
     ### PARTIE TEXTE BRUT (POUR TEST) ###
 
-    #print(tp_text)
-    #print(doc.metadata)
+    # print(tp_text)
+    # print(doc.metadata)
 
     ### PARTIE NOM FICHIER ###
 
     print("Filename:")
     path = sys.argv[1]
     basename = os.path.basename(path)
-    print(file+'\n')
+    print(file + '\n')
 
     ### PARTIE TITRE ###
 
     print("Title:")
     title = doc.metadata["title"]
     if title == "":
-        patternTitle = "^\A(.*)$"
+        patternTitle = "^\A(.*)"
         title = re.search(patternTitle, tp_text)
-    print(title)
+        print(title.group(0))
+    else:
+        print(title)
+    print('\n')
 
     ### PARTIE AUTEURS ###
 
     print("Author:")
-    print(doc.metadata["author"] + '\n')
+    author = doc.metadata["author"]
+    if author is None or author == "":
+        patternAuthors = "([\w.\-]+@[\w.\-]+[.][a-zA-Z]{2,4})"
+
+        if re.search(patternAuthors, tp_text) is not None:
+            authors_email = re.findall(patternAuthors, tp_text)
+            for author in authors_email:
+                email_decompose = author[0:author.find('@')]
+
+                if email_decompose.find('.') != -1:
+                    nom = email_decompose[0:email_decompose.find('.')]
+                    prenom = email_decompose[email_decompose.find('.') + 1:]
+                    print(nom, prenom, " ; ", end="")
+                else:
+                    print(email_decompose, "; ", end="")
+            print('\n')
+        else:
+            print("Auteurs non trouvés \n")
+    else:
+        print(author)
+        print('\n')
 
     ### PARTIE ABSTRACT ###
-
     patternAbstract = "(Abstract(-|.|\n))((.|\n)*)(?=(1(\n| )Introduction)|(I. INTRODUCTION))"
 
     print("Abstract:")
-    if re.search(patternAbstract, tp_text) != None:
+    if re.search(patternAbstract, tp_text) is not None:
         abstract = re.search(patternAbstract, tp_text).group(3)
         print(abstract)
         print('\n')
