e553a3ef99ed16be6391ca138e567fb0e4704465
guilhemmazoyer <guilhem.mazoyer@laposte.net>
2022-03-05 23:37:15 +0100

    Placement des premières briques

--- fileManager.py
+++ fileManager.py
@@ -1,23 +1,38 @@
 import fitz
-import os,sys
+import os, sys, shutil
 
 class fileManager:
     folder = ""
     resFolder = ""
+    option = ""
+    index = 0
     files = []
 
-    def __init__(self, folder):
+    def __init__(self, folder, option):
         self.folder = folder
-        self.resFolder = folder + "/result"
+        self.option = option
         files = os.listdir(self.folder)
         files = filter(lambda f: f.endswith(('.pdf', '.PDF')), files)
         self.files = list(files)
 
-    def writeTxt(self, txt_basename, filename_display, title_display , author_display, abstract_display):
-        # Creation repertoire resultat
-        if not os.path.isdir(self.resFolder):
-            os.mkdir(self.resFolder)
+    # Creer un dossier "result" dans le dossier source
+    def createResultFolder(self):
+        if os.path.exist(self.resFolder):
+            shutil.rmtree(self.resFolder)
+        os.mkdir(self.resFolder)
 
+    # 
+    def createResultFileTXT(self):
+        # TODO
+        print("")
+    
+    #
+    def createResultFileXML(self):
+        # TODO
+        print("")
+
+    #
+    def writeTxt(self, txt_basename, filename_display, title_display , author_display, abstract_display):
         # Creation et ouverture du fichier .txt
         txtFileToFill = open(self.resFolder + '/' + txt_basename + ".txt", "w+")
 
@@ -26,6 +41,16 @@ class fileManager:
         txtFileToFill.write(title_display)
         txtFileToFill.write(author_display)
         txtFileToFill.write(abstract_display)
+
+        # Fermeture du fichier
+        txtFileToFill.close()
+
+    #
+    def writeXML(self, txt_basename, filename_display, title_display , author_display, abstract_display):
+        # Creation et ouverture du fichier .xml
+        txtFileToFill = open(self.resFolder + '/' + txt_basename + ".xml", "w+")
+
+        # TODO
 
         # Fermeture du fichier
         txtFileToFill.close()
@@ -50,9 +75,26 @@ class fileManager:
         for file in self.files:
             doc = fitz.open(sys.argv[1] + '/' + file)
             metadatas.append(doc.metadata)
-            index += 1
+            self.index += 1
 
         return metadatas
 
     def getFiles(self):
         return self.files
+    
+    def getIndex(self):
+        return self.index
+    
+    def pdfFilesProcessing(self):
+        # Créer le dossier result
+        self.resFolder = self.folder + "/result"
+        self.createResultFolder(self)
+
+        # TODO
+
+    def pdfFilesProcessingWindows(self):
+         # Créer le dossier result
+        self.resFolder = self.folder + "\result"
+        self.createResultFolder(self)
+
+        # TODO
