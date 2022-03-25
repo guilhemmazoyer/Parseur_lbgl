36196c0593e6a2dffa7e36089ab1b6e9e7d8c569
guilhemmazoyer <guilhem.mazoyer@laposte.net>
2022-03-25 20:47:26 +0100

    Ajout du choix de langue

---
+++ language/lang.py
@@ -0,0 +1,42 @@
+# Ce fichier contient tous les noms et les traductions des éléments du GUI
+# Les noms sont écris sous la forme :
+# varname = ["english", "french"]
+
+    ## Accelerators
+MENU_FILES_OPEN_FOLDER_A = "Ctrl+O"
+
+    ## Menus
+MENU_FILES_OPEN_FOLDER = ["Open Folder...", "Ouvrir un Dossier..."]
+MENU_FILES_SET_DEFAULT_PRESEST = ["Set Default Presets", "Appliquer les paramètres par défaut"]
+MENU_FILES_EXIT = ["Exit", "Quitter"]
+MENU_FILES_TITLE = ["File", "Fichier"]
+MENU_LANGUAGE_TITLE = ["Language", "Langue"]
+
+    ## File Selection
+LABELFRAME_FILE_SELECTION = ["Files selection", "Sélection des fichiers"]
+BUTTON_SELECT_ALL = ["Select all", "Tout sélectionner"]
+BUTTON_UNSELECT_ALL = ["Unselect all", "Tout désélectionner"]
+
+    ## Presets
+LABELFRAME_PRESETS_SELECTION = ["Presets selection", "Sélection des paramètres"]
+CHECK_FILENAME = ["File name", "Nom du fichier"]
+CHECK_TITLE = ["Title", "Titre"]
+CHECK_AUTHOR = ["Authors", "Auteurs"]
+CHECK_ABSTRACT = ["Abstract", "Abstract"]
+CHECK_INTRODUCTION = ["Introduction", "Introduction"]
+CHECK_CORPS = ["Corps", "Corps"]
+CHECK_CONCLUSION = ["Conclusion", "Conclusion"]
+CHECK_DISCUSSION = ["Discussion", "Discussion"]
+CHECK_REFERENCES = ["Reference", "Bibliographie"]
+
+    ## Others UI Elements
+RADIO_XML_FORMAT = ["XML Format", "Format XML"]
+RADIO_TXT_FORMAT = ["TXT Format", "Format TXT"]
+BUTTON_LAUNCH_PARSING = ["Start parsing", "Lancement du parsing"]
+
+class lang:
+
+    LANG = 0
+
+    def __init__(self) -> None:
+        pass
--- programGUI.py
+++ programGUI.py
@@ -1,5 +1,8 @@
+from faulthandler import disable
 from tkinter import *
 from tkinter.filedialog import askdirectory
+from language.lang import lang
+from language.lang import *
 
 root = Tk()
 root.title("PDF Parser")
@@ -13,7 +16,8 @@ def launch():
 
 def askDirectoryToParse():
     parseFolder = askdirectory()
-    butOpenFolder.destroy()
+    if parseFolder != "":
+        butOpenFolder.destroy()
     # TODO creer l'affichage des fichiers pdf du dossier selectionne
     print(parseFolder)
 
@@ -32,85 +36,123 @@ def resetGUIPreset():
     # Output preset
     valueCheckXmlOrTxt.set(0)
 
+def updateLanguageDisplay():
+    ## Menus
+    menubar.entryconfig(index=1, label=MENU_FILES_TITLE[lang.LANG])
+    filemenu.entryconfig(index=0, label=MENU_FILES_OPEN_FOLDER[lang.LANG])
+    filemenu.entryconfig(index=1, label=MENU_FILES_SET_DEFAULT_PRESEST[lang.LANG])
+    filemenu.entryconfig(index=3, label=MENU_FILES_EXIT[lang.LANG])
+    menubar.entryconfig(index=2, label=MENU_LANGUAGE_TITLE[lang.LANG])
+
+    ## File Selection
+    fileFrame.config(text=LABELFRAME_FILE_SELECTION[lang.LANG])
+    butSelectAll.config(text=BUTTON_SELECT_ALL[lang.LANG])
+    butUnselectAll.config(text=BUTTON_UNSELECT_ALL[lang.LANG])
+    butOpenFolder.config(text=MENU_FILES_OPEN_FOLDER[lang.LANG])
+
+    ## Presets
+    presetFrame.config(text=LABELFRAME_PRESETS_SELECTION[lang.LANG])
+    checkFilename.config(text=CHECK_FILENAME[lang.LANG])
+    checkTitle.config(text=CHECK_TITLE[lang.LANG])
+    checkAuthor.config(text=CHECK_AUTHOR[lang.LANG])
+    checkAbstract.config(text=CHECK_ABSTRACT[lang.LANG])
+    checkIntroduction.config(text=CHECK_INTRODUCTION[lang.LANG])
+    checkCorps.config(text=CHECK_CORPS[lang.LANG])
+    checkConclusion.config(text=CHECK_CONCLUSION[lang.LANG])
+    checkDiscussion.config(text=CHECK_DISCUSSION[lang.LANG])
+    checkBiblio.config(text=CHECK_REFERENCES[lang.LANG])
+
+    ## Others UI Elements
+    radXMLFormat.config(text=RADIO_XML_FORMAT[lang.LANG])
+    radTXTFormat.config(text=RADIO_TXT_FORMAT[lang.LANG])
+    butLaunch.config(text=BUTTON_LAUNCH_PARSING[lang.LANG])
+
+def changeToEnglish():
+    lang.LANG = 0
+    updateLanguageDisplay()
+
+def changeToFrench():
+    lang.LANG = 1
+    updateLanguageDisplay()
 
     ## Menu avec choix de langue et de dossier ##
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Open Folder...", accelerator='ctrl+o', command=askDirectoryToParse)
-filemenu.add_command(label="Set Default Preset", command=resetGUIPreset)
+filemenu.add_command(accelerator='ctrl+o', command=askDirectoryToParse)
+filemenu.add_command(command=resetGUIPreset)
 filemenu.add_separator()
-filemenu.add_command(label="Exit", command=root.quit)
+filemenu.add_command(command=root.quit)
 
-menubar.add_cascade(label="Window", menu=filemenu)
+menubar.add_cascade(menu=filemenu)
 
 langmenu = Menu(menubar, tearoff=0)
-langmenu.add_command(label="Français", command=launch)
-langmenu.add_command(label="English", command=resetGUIPreset)
+langmenu.add_command(label="English", command=changeToEnglish)
+langmenu.add_command(label="Français", command=changeToFrench)
 
-menubar.add_cascade(label="Language", menu=langmenu)
+menubar.add_cascade(menu=langmenu)
 
     ## Frame constituee des fichiers pdf ##
-fileFrame = LabelFrame(root, text="Files selection", padx=3, pady=3)
+fileFrame = LabelFrame(root, padx=3, pady=3)
 fileFrame.grid(row=0, column=0, padx=10, pady=10, sticky=NW)
 
-butSelectAll = Button(fileFrame, text="Select all")
+butSelectAll = Button(fileFrame)
 butSelectAll.grid(row=0, column=0, padx=3, pady=5)
 
-butUnselectAll = Button(fileFrame, text="Unselect all")
+butUnselectAll = Button(fileFrame)
 butUnselectAll.grid(row=0, column=1, padx=3, pady=5)
 
 space = Label(fileFrame, text="")
 space.grid(row=1, column=2, padx=3, pady=5)
 
-butOpenFolder = Button(fileFrame, text="Open Folder...", command=askDirectoryToParse, bg="#FFF4E5", activebackground='green', activeforeground='white', font=1, borderwidth=2, relief="groove")
+butOpenFolder = Button(fileFrame, command=askDirectoryToParse, bg="#FFF4E5", activebackground='green', activeforeground='white', font=1, borderwidth=2, relief="groove")
 butOpenFolder.grid(row=2, column=1, padx=3, pady=5)
 
     ## Frame constituee des presets pour le parser ##
-presetFrame = LabelFrame(root, text="Presets selection", padx=5, pady=5)
+presetFrame = LabelFrame(root, padx=5, pady=5)
 presetFrame.grid(row=0, column=1, padx=10, pady=10, sticky=NE)
 
 valueCheckFilemane = IntVar()
-checkFilename = Checkbutton(presetFrame, text="Nom du fichier", variable=valueCheckFilemane)
+checkFilename = Checkbutton(presetFrame, variable=valueCheckFilemane)
 checkFilename.select()
 checkFilename.grid(row=0, sticky=W)
 
 valueCheckTitle = IntVar()
-checkTitle = Checkbutton(presetFrame, text="Titre de l'article", variable=valueCheckTitle)
+checkTitle = Checkbutton(presetFrame, variable=valueCheckTitle)
 checkTitle.select()
 checkTitle.grid(row=1, sticky=W)
 
 valueCheckAuthor = IntVar()
-checkAuthor = Checkbutton(presetFrame, text="Auteur", variable=valueCheckAuthor)
+checkAuthor = Checkbutton(presetFrame, variable=valueCheckAuthor)
 checkAuthor.select()
 checkAuthor.grid(row=2, sticky=W)
 
 valueCheckAbstract = IntVar()
-checkAbstract = Checkbutton(presetFrame, text="Abstract", variable=valueCheckAbstract)
+checkAbstract = Checkbutton(presetFrame, variable=valueCheckAbstract)
 checkAbstract.select()
 checkAbstract.grid(row=3, sticky=W)
 
 valueCheckIntroduction = IntVar()
-checkIntroduction = Checkbutton(presetFrame, text="Introduction", variable=valueCheckIntroduction)
+checkIntroduction = Checkbutton(presetFrame, variable=valueCheckIntroduction)
 checkIntroduction.select()
 checkIntroduction.grid(row=4, sticky=W)
 
 valueCheckCorps = IntVar()
-checkCorps = Checkbutton(presetFrame, text="Corps", variable=valueCheckCorps)
+checkCorps = Checkbutton(presetFrame, variable=valueCheckCorps)
 checkCorps.select()
 checkCorps.grid(row=5, sticky=W)
 
 valueCheckConclusion = IntVar()
-checkConclusion = Checkbutton(presetFrame, text="Conclusion", variable=valueCheckConclusion)
+checkConclusion = Checkbutton(presetFrame, variable=valueCheckConclusion)
 checkConclusion.select()
 checkConclusion.grid(row=6, sticky=W)
 
 valueCheckDiscussion = IntVar()
-checkDiscussion = Checkbutton(presetFrame, text="Discussion", variable=valueCheckDiscussion)
+checkDiscussion = Checkbutton(presetFrame, variable=valueCheckDiscussion)
 checkDiscussion.select()
 checkDiscussion.grid(row=7, sticky=W)
 
 valueCheckBiblio = IntVar()
-checkBiblio = Checkbutton(presetFrame, text="Bibliographie", variable=valueCheckBiblio)
+checkBiblio = Checkbutton(presetFrame, variable=valueCheckBiblio)
 checkBiblio.select()
 checkBiblio.grid(row=8, sticky=W)
 
@@ -121,10 +163,12 @@ launchFrame.grid(row=1, column=0, sticky=SW)
 
 valueCheckXmlOrTxt = IntVar()
 
-Radiobutton(launchFrame, text="Format XML", variable=valueCheckXmlOrTxt, value=0).grid(row=0, column=0)
-Radiobutton(launchFrame, text="Format TXT", variable=valueCheckXmlOrTxt, value=1).grid(row=0, column=1)
+radXMLFormat = Radiobutton(launchFrame, variable=valueCheckXmlOrTxt, value=0)
+radXMLFormat.grid(row=0, column=0)
+radTXTFormat = Radiobutton(launchFrame, variable=valueCheckXmlOrTxt, value=1)
+radTXTFormat.grid(row=0, column=1)
 
-butLaunch = Button(launchFrame, text="Start parsing", command=launch)
+butLaunch = Button(launchFrame, command=launch)
 butLaunch.grid(row=0, column=2)
 
 
@@ -134,4 +178,7 @@ labError.grid(row=1, column=1, sticky=SE)
 
 
 root.config(menu=menubar)
+
+updateLanguageDisplay();
+
 root.mainloop()
